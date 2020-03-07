d42dbfe09ad415dc389dcee16038435a106d1638
ApoChagi <56980206+ApoChagi@users.noreply.github.com>
2020-03-07 18:13:14 +0200

    Update SerialPort.hpp

--- c++midman/SerialPort.hpp
+++ c++midman/SerialPort.hpp
@@ -1,23 +1,28 @@
-#define ARDUINO_WAIT_TIME 1000
-#define MAX_DATA_LENGHT 255
+#ifndef SERIALPORT_H
+#define SERIALPORT_H
 
-#pragma once
+#define ARDUINO_WAIT_TIME 2000
+#define MAX_DATA_LENGTH 255
 
 #include <windows.h>
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-class SerialPort{
-  private:
+class SerialPort
+{
+private:
     HANDLE handler;
     bool connected;
     COMSTAT status;
     DWORD errors;
-  public:
-    explicit SerialPort(const char *portName);
+public:
+    SerialPort(char *portName);
     ~SerialPort();
 
-    int readSerialPort(const char *buffer, unsigned int buf_size);
-    bool writeSerialPort(const char *buffer, int buf_size);
+    int readSerialPort(char *buffer, unsigned int buf_size);
+    bool writeSerialPort(char *buffer, unsigned int buf_size);
     bool isConnected();
-    void closeSerial();
 };
+
+#endif // SERIALPORT_H
+
